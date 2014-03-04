bcc88519cb9b2378bb9e808e659a624631441bc8
Bill Hunt <bill@krues8dr.com>
2014-03-04 11:48:23 -0500

    Updating news page.

--- wp-content/themes/bones/index.php
+++ wp-content/themes/bones/index.php
@@ -1,68 +1,70 @@
 <?php get_header(); ?>
 
-	<div id="content" class="container">
+<!-- <?php echo basename(__FILE__) ?> -->
 
-		<div id="inner-content" class="">
+<div id="content" class="container">
 
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div id="inner-content" class="col-sm-8">
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-				<header class="article-header">
+		<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article">
 
-					<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-					<p class="byline vcard"><?php
-						printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
-					?></p>
+			<header class="article-header">
 
-				</header>
+				<h1 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+				<p class="byline vcard"><?php
+					printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
+				?></p>
 
-				<section class="entry-content clearfix">
-					<?php the_content(); ?>
-				</section>
+			</header>
 
-				<footer class="article-footer">
-					<p class="tags"><?php the_tags( '<span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?></p>
+			<section class="entry-content clearfix">
+				<?php the_excerpt(); ?>
+			</section>
 
-				</footer>
+			<footer class="article-footer">
+				<p class="tags"><?php the_tags( '<span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '' ); ?></p>
 
-				<?php // comments_template(); // uncomment if you want to use them ?>
+			</footer>
 
-			</article>
+			<?php // comments_template(); // uncomment if you want to use them ?>
 
-			<?php endwhile; ?>
+		</article>
 
-					<?php if ( function_exists( 'bones_page_navi' ) ) { ?>
-							<?php bones_page_navi(); ?>
-					<?php } else { ?>
-							<nav class="wp-prev-next">
-									<ul class="clearfix">
-										<li class="prev-link"><?php next_posts_link( __( '&laquo; Older Entries', 'bonestheme' )) ?></li>
-										<li class="next-link"><?php previous_posts_link( __( 'Newer Entries &raquo;', 'bonestheme' )) ?></li>
-									</ul>
-							</nav>
-					<?php } ?>
+		<?php endwhile; ?>
 
-			<?php else : ?>
+				<?php if ( function_exists( 'bones_page_navi' ) ) { ?>
+						<?php bones_page_navi(); ?>
+				<?php } else { ?>
+						<nav class="wp-prev-next">
+								<ul class="clearfix">
+									<li class="prev-link"><?php next_posts_link( __( '&laquo; Older Entries', 'bonestheme' )) ?></li>
+									<li class="next-link"><?php previous_posts_link( __( 'Newer Entries &raquo;', 'bonestheme' )) ?></li>
+								</ul>
+						</nav>
+				<?php } ?>
 
-					<article id="post-not-found" class="hentry clearfix">
-							<header class="article-header">
-								<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-						</header>
-							<section class="entry-content">
-								<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-						</section>
-						<footer class="article-footer">
-								<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
-						</footer>
-					</article>
+		<?php else : ?>
 
-			<?php endif; ?>
+				<article id="post-not-found" class="hentry clearfix">
+						<header class="article-header">
+							<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+					</header>
+						<section class="entry-content">
+							<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+					</section>
+					<footer class="article-footer">
+							<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+					</footer>
+				</article>
 
-		</div>
-
-		<?php get_sidebar(); ?>
+		<?php endif; ?>
 
 	</div>
+
+	<?php get_sidebar(); ?>
+
+</div>
 
 <?php get_footer(); ?>
