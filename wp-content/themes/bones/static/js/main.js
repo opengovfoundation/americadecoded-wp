a9feb091c173482a4481bfe7c2a1632fddaa66f9
Bill Hunt <bill@krues8dr.com>
2014-03-04 13:15:44 -0500

    Tweaking map locations.

--- wp-content/themes/bones/static/js/main.js
+++ wp-content/themes/bones/static/js/main.js
@@ -3,17 +3,19 @@ var map, paper, pageWidth = 800;
 var drawIt = function(){
 	if($('#plot-map-container').length > 0) {
 		paper = new ScaleRaphael('plot-map-container', 950, 650);
-		console.log(paper);
+
 		map = paper.USMap();
 		paper.scaleAll(pageWidth/map.width);
 
 
 		$.getJSON(urlbase + 'js/instances.json', function(data){
 			instances = data.features;
-
+console.log(instances);
 			$.each(instances, function(){
-				map.plot(this.geometry.coordinates[0], this.geometry.coordinates[1], this.geometry.properties["site-name"], this.geometry.properties["site-link"]);
-				map.darkenState(this.geometry.properties["place-state"], .1);
+				map.plot(this.geometry.coordinates[0], this.geometry.coordinates[1], this.geometry.properties["site-name"], this.geometry.properties["site-link"], this.geometry.properties["status"]);
+				if(this.geometry.properties["place-type"] == "state") {
+					map.darkenState(this.geometry.properties["place-state"], .1);
+				}
 			});
 		});
 	}
