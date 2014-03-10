02eceadbc14b4c515dcccda50b1e87422f966caa
Bill Hunt <bill@krues8dr.com>
2014-03-10 16:05:33 -0400

    Adding Google Analytics tracking.

--- wp-content/themes/bones/footer.php
+++ wp-content/themes/bones/footer.php
@@ -47,7 +47,16 @@
         </footer>
 		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+          ga('create', 'UA-42400665-6', 'americadecoded.org');
+          ga('send', 'pageview');
+
+        </script>
 	</body>
 
 </html>
